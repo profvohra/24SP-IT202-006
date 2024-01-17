3a1d83d9efe4ec6eb5922bb527acbd5955dae32c
Rosemina Vohra <rosemina.vohra@gmail.com>
2024-01-17 18:48:31 -0500

    Update my_goal.php

--- my_goal.php
+++ my_goal.php
@@ -1,6 +1,4 @@
 <?php
- echo "My Goals for IT-202 is teach you all to be great web developers.";
- echo "<br>";
- echo "Another goal.";
+ echo "My Goals for IT-202 is to teach you all to be great web developers.";
 ?>
  
